814b23d346659bf3ad82420f5b9fb4c799de2d07
avenirs05 <avenirs05@gmail.com>
2018-02-12 00:20:11 +0200

    version color with rectangles

--- index.php
+++ index.php
@@ -14,28 +14,32 @@
 
   <body> 
 
-  	<div class="container-fluid sticky-top header d-none d-lg-block">			
-				<nav class="row navbar navbar-light bg-dark">
-					<div class="ml-auto mr-auto" style="padding-left: 0 auto; width: 1140px;">
-				    <div class="col-auto d-flex align-items-center">
-				      <a href="/"><img src="/imgs/logo.png" height="40" alt=""></a>
-				    </div>
-				    <ul class="col d-flex align-items-center list-unstyled list-inline">
-				      <li class="list-inline-item"><a href="/#why-uber-section" id="why-uber-link">Почему UBER</a></li>
-				      <li class="list-inline-item"><a href="/#req-drivers-section" id="req-drivers-link">Требования к водителям</a></li>
-				      <li class="list-inline-item"><a href="/#req-auto-section" id="req-auto-link">Tребования к авто</a></li>
-				      <li class="list-inline-item"><a href="/#our-commission-section" id="our-commission-link">Наша комиссия</a></li>
-				      <li class="list-inline-item"><a href="/#change-partner-section" id="change-partner-link">Сменить партнера</a></li> 
-				    </ul>
-				    <div class="col-auto d-flex align-items-center justify-content-end contacts">           
-				      <img class="phone-icon" src="imgs/phone.svg" width="17" height="17" alt="">
-				      <div class="phone-digits">+380 99 291 89 60</div>
-				      <img class="whatsapp-icon" src="imgs/whatsapp.svg" width="18" height="18" alt="">
-				      <img class="viber-icon" src="imgs/viber.png" width="16" height="18" alt="">
-				      <img src="imgs/telegram.png" width="18" height="18" alt="">           
-				    </div>
-				  </div>
-				</nav>			
+  	<div class="container-fluid header d-none d-lg-block">			
+			<nav class="row navbar navbar-light">
+				<div class="ml-auto mr-auto" style="padding-left: 0 auto; width: 1140px;">
+			    <div class="col-auto d-flex align-items-center">
+			      <a href="/">
+			      	<img src="/imgs/logo.png" height="90" alt="">
+			      </a>
+			      <div id="title-wrap">
+			      	<span id="company-name">Моё окошко</span><br>
+							<span id="what-sell">Комплектующие для окон</span>
+			      </div>
+			      <ul class="col d-flex align-items-center list-unstyled list-inline">
+			        <li class="list-inline-item"><a href="/#why-uber-section" id="why-uber-link">Главная</a></li>
+			        <li class="list-inline-item"><a href="/#req-drivers-section" id="req-drivers-link">Доставка</a></li>
+			        <li class="list-inline-item"><a href="/#req-auto-section" id="req-auto-link">Прайс-лист</a></li>
+			      </ul>
+			      <div class="col-auto d-flex align-items-center justify-content-end contacts">           
+			        <img class="phone-icon" src="imgs/phone.svg" width="17" height="17" alt="">
+			        <div class="phone-digits">+7 977 270 22 34</div>
+			        <img class="whatsapp-icon" src="imgs/whatsapp.svg" width="18" height="18" alt="">
+			        <img class="viber-icon" src="imgs/viber.png" width="16" height="18" alt="">
+			        <img src="imgs/telegram.png" width="18" height="18" alt="">           
+			      </div>
+			    </div>
+			  </div>
+			</nav>			
   	</div>
 
 		<div class="dashboard display-animation" style="margin: 0 auto; width: 1130px;">
@@ -49,7 +53,7 @@
 		      </span>      
 		    </span>
 		  </a>
-		  <a class="tile tile-lg tile-sqr tile-purple ripple-effect" href="#">
+		  <a class="tile tile-lg tile-sqr tile-purple ripple-effect" href="moskit.php" target="_blank">
 		    <span class="content-wrapper">
 		      <span class="tile-content">
 		        <span class="tile-img" style="background-image: url(imgs/tiles/moskitka.jpg);"></span>

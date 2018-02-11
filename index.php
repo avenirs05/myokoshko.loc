5c0df501cd421e603d20f9e1a075e6984448c414
avenirs05 <avenirs05@gmail.com>
2018-02-11 20:47:38 +0200

    first

---
+++ index.php
@@ -0,0 +1,125 @@
+﻿<!doctype html>
+<html>
+  <head>
+    <title>Калькулятор расчета стоимости москитной сетки</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> 
+
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+
+    <link href="css/tiles.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+  </head>
+
+  <body> 
+
+		<div class="dashboard display-animation" style="margin: 0 auto; width: 1130px;">
+		  <a class="tile tile-lg tile-pink ripple-effect" href="#">
+		    <span class="content-wrapper">
+		      <span class="tile-content">
+		        <span class="tile-img" style="background-image: url('imgs/tiles/otkosi.jpg');"></span>
+		        <span class="tile-holder tile-holder-sm">
+		          <span class="title">Откосы</span>
+		        </span>
+		      </span>      
+		    </span>
+		  </a>
+		  <a class="tile tile-lg tile-sqr tile-purple ripple-effect" href="#">
+		    <span class="content-wrapper">
+		      <span class="tile-content">
+		        <span class="tile-img" style="background-image: url(imgs/tiles/moskitka.jpg);"></span>
+		        <span class="tile-holder tile-holder-sm">
+		          <span class="title">Москитные сетки</span>
+		        </span>
+		      </span>      
+		    </span>
+		  </a>
+		  <a class="tile tile-lg tile-sqr tile-cyan ripple-effect" href="#">
+		    <span class="content-wrapper">
+		      <span class="tile-content">
+		        <span class="tile-img" style="background-image: url(https://www.google.com/design/images/principles.png);"></span>
+		        <span class="tile-holder tile-holder-sm">
+		          <span class="title">Подоконники <br> Ремонтные накладки</span>
+		        </span>
+		      </span>      
+		    </span>
+		  </a>
+		  <a class="tile tile-lg tile-sqr tile-amber ripple-effect" href="#">
+		    <span class="content-wrapper">
+		      <span class="tile-content">
+		        <span class="tile-img tile-img-bg" style="background-image: url(https://www.google.com/design/images/doodlearchive.png);"></span>
+		        <span class="tile-holder tile-holder-sm">
+		          <span class="title">Материалы для монтажа</span>
+		        </span>
+		      </span>      
+		    </span>
+		  </a>
+		  <a class="tile tile-lg tile-light-blue ripple-effect" href="#">
+		    <span class="content-wrapper">
+		      <span class="tile-content">
+		        <span class="tile-img tile-img-bg" style="background-image: url(https://www.google.com/design/images/googleplus.png);"></span>
+		        <span class="tile-holder tile-holder-sm">
+		          <span class="title">Уплотнители</span>
+		        </span>
+		      </span>      
+		    </span>
+		  </a>
+		  <a class="tile tile-lg tile-sqr tile-red ripple-effect" href="#">
+		    <span class="content-wrapper">
+		      <span class="tile-content">
+		        <span class="tile-img" style="background-image: url(https://www.google.com/design/images/design-minutes.png);"></span>
+		        <span class="tile-holder tile-holder-sm">
+		          <span class="title">Фурнитура <br> Ручки / Накладки</span>
+		        </span>
+		      </span>      
+		    </span>
+		  </a>
+		  <a class="tile tile-lg tile-sqr tile-indigo ripple-effect" href="#">
+		    <span class="content-wrapper">
+		      <span class="tile-content">
+		        <span class="tile-img" style="background-image: url(https://www.google.com/design/images/cast.png);"></span>
+		        <span class="tile-holder tile-holder-sm">
+		          <span class="title">Отливы / Козырьки</span>
+		        </span>
+		      </span>      
+		    </span>
+		  </a>
+		  <a class="tile tile-lg tile-sqr tile-deep-orange ripple-effect" href="#">
+		    <span class="content-wrapper">
+		      <span class="tile-content">
+		        <span class="tile-img" style="background-image: url(https://www.google.com/design/images/glass.png);"></span>
+		        <span class="tile-holder tile-holder-sm">
+		          <span class="title">Панели ПВХ <br>Сайдинг / Ламинат</span>
+		        </span>
+		      </span>      
+		    </span>
+		  </a>
+		  <a class="tile tile-lg tile-light-green ripple-effect" href="#">
+		    <span class="content-wrapper">
+		      <span class="tile-content">
+		        <span class="tile-img tile-img-bg" style="background-image: url(https://www.google.com/design/images/webdesigner.png);"></span>
+		        <span class="tile-holder tile-holder-sm">
+		          <span class="title">Пиломатериалы</span>
+		        </span>
+		      </span>      
+		    </span>
+		  </a>
+		</div>
+
+
+	
+	<script src="js/jquery-3.1.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+	<!-- Tiles -->
+	<script src="js/tiles.js"></script>
+
+
+	<!-- My Own Js-scripts -->
+	<script src="js/main.js"></script>
+
+
+	</body>	
+	</html>
+

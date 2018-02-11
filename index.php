f67c851d8ce435d595c52ee49c22cdf809a8becc
avenirs05 <avenirs05@gmail.com>
2018-02-11 22:58:46 +0200

    last

--- index.php
+++ index.php
@@ -14,6 +14,30 @@
 
   <body> 
 
+  	<div class="container-fluid sticky-top header d-none d-lg-block">			
+				<nav class="row navbar navbar-light bg-dark">
+					<div class="ml-auto mr-auto" style="padding-left: 0 auto; width: 1140px;">
+				    <div class="col-auto d-flex align-items-center">
+				      <a href="/"><img src="/imgs/logo.png" height="40" alt=""></a>
+				    </div>
+				    <ul class="col d-flex align-items-center list-unstyled list-inline">
+				      <li class="list-inline-item"><a href="/#why-uber-section" id="why-uber-link">Почему UBER</a></li>
+				      <li class="list-inline-item"><a href="/#req-drivers-section" id="req-drivers-link">Требования к водителям</a></li>
+				      <li class="list-inline-item"><a href="/#req-auto-section" id="req-auto-link">Tребования к авто</a></li>
+				      <li class="list-inline-item"><a href="/#our-commission-section" id="our-commission-link">Наша комиссия</a></li>
+				      <li class="list-inline-item"><a href="/#change-partner-section" id="change-partner-link">Сменить партнера</a></li> 
+				    </ul>
+				    <div class="col-auto d-flex align-items-center justify-content-end contacts">           
+				      <img class="phone-icon" src="imgs/phone.svg" width="17" height="17" alt="">
+				      <div class="phone-digits">+380 99 291 89 60</div>
+				      <img class="whatsapp-icon" src="imgs/whatsapp.svg" width="18" height="18" alt="">
+				      <img class="viber-icon" src="imgs/viber.png" width="16" height="18" alt="">
+				      <img src="imgs/telegram.png" width="18" height="18" alt="">           
+				    </div>
+				  </div>
+				</nav>			
+  	</div>
+
 		<div class="dashboard display-animation" style="margin: 0 auto; width: 1130px;">
 		  <a class="tile tile-lg tile-pink ripple-effect" href="#">
 		    <span class="content-wrapper">
@@ -35,30 +59,30 @@
 		      </span>      
 		    </span>
 		  </a>
-		  <a class="tile tile-lg tile-sqr tile-cyan ripple-effect" href="#">
+		  <a class="tile tile-lg tile-sqr tile-teal ripple-effect" href="#">
 		    <span class="content-wrapper">
 		      <span class="tile-content">
-		        <span class="tile-img" style="background-image: url(https://www.google.com/design/images/principles.png);"></span>
+		        <span class="tile-img" style="background-image: url(imgs/tiles/podokonniki.jpg);"></span>
 		        <span class="tile-holder tile-holder-sm">
 		          <span class="title">Подоконники <br> Ремонтные накладки</span>
 		        </span>
 		      </span>      
 		    </span>
 		  </a>
-		  <a class="tile tile-lg tile-sqr tile-amber ripple-effect" href="#">
+		  <a class="tile tile-lg tile-sqr tile-teal ripple-effect" href="#">
 		    <span class="content-wrapper">
 		      <span class="tile-content">
-		        <span class="tile-img tile-img-bg" style="background-image: url(https://www.google.com/design/images/doodlearchive.png);"></span>
+		        <span class="tile-img" style="background-image: url(imgs/tiles/materials.jpg);"></span>
 		        <span class="tile-holder tile-holder-sm">
 		          <span class="title">Материалы для монтажа</span>
 		        </span>
 		      </span>      
 		    </span>
 		  </a>
-		  <a class="tile tile-lg tile-light-blue ripple-effect" href="#">
+		  <a class="tile tile-lg tile-blue ripple-effect" href="#">
 		    <span class="content-wrapper">
 		      <span class="tile-content">
-		        <span class="tile-img tile-img-bg" style="background-image: url(https://www.google.com/design/images/googleplus.png);"></span>
+		        <span class="tile-img" style="background-image: url(imgs/tiles/uplotniteli.jpg);"></span>
 		        <span class="tile-holder tile-holder-sm">
 		          <span class="title">Уплотнители</span>
 		        </span>
@@ -68,7 +92,7 @@
 		  <a class="tile tile-lg tile-sqr tile-red ripple-effect" href="#">
 		    <span class="content-wrapper">
 		      <span class="tile-content">
-		        <span class="tile-img" style="background-image: url(https://www.google.com/design/images/design-minutes.png);"></span>
+		        <span class="tile-img" style="background-image: url(imgs/tiles/furnitura.jpg);"></span>
 		        <span class="tile-holder tile-holder-sm">
 		          <span class="title">Фурнитура <br> Ручки / Накладки</span>
 		        </span>
@@ -78,7 +102,7 @@
 		  <a class="tile tile-lg tile-sqr tile-indigo ripple-effect" href="#">
 		    <span class="content-wrapper">
 		      <span class="tile-content">
-		        <span class="tile-img" style="background-image: url(https://www.google.com/design/images/cast.png);"></span>
+		        <span class="tile-img" style="background-image: url(imgs/tiles/otlivi.jpg);"></span>
 		        <span class="tile-holder tile-holder-sm">
 		          <span class="title">Отливы / Козырьки</span>
 		        </span>
@@ -88,17 +112,17 @@
 		  <a class="tile tile-lg tile-sqr tile-deep-orange ripple-effect" href="#">
 		    <span class="content-wrapper">
 		      <span class="tile-content">
-		        <span class="tile-img" style="background-image: url(https://www.google.com/design/images/glass.png);"></span>
+		        <span class="tile-img" style="background-image: url(imgs/tiles/paneli-pvh.jpg);"></span>
 		        <span class="tile-holder tile-holder-sm">
 		          <span class="title">Панели ПВХ <br>Сайдинг / Ламинат</span>
 		        </span>
 		      </span>      
 		    </span>
 		  </a>
-		  <a class="tile tile-lg tile-light-green ripple-effect" href="#">
+		  <a class="tile tile-lg tile-green ripple-effect" href="#">
 		    <span class="content-wrapper">
 		      <span class="tile-content">
-		        <span class="tile-img tile-img-bg" style="background-image: url(https://www.google.com/design/images/webdesigner.png);"></span>
+		        <span class="tile-img" style="background-image: url(imgs/tiles/pilomaterialy.jpg);"></span>
 		        <span class="tile-holder tile-holder-sm">
 		          <span class="title">Пиломатериалы</span>
 		        </span>

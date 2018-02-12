1f63d355d62e7950c092a4cbe8c679eb16b31e54
avenirs05 <avenirs05@gmail.com>
2018-02-13 00:14:55 +0200

    last

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 ﻿<!doctype html>
 <html>
   <head>
-    <title>Калькулятор расчета стоимости москитной сетки</title>
+    <title>Комплектующие для окон</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> 
@@ -19,23 +19,45 @@
 				<div id="header-content" class="ml-auto mr-auto">
 			    <div class="col-auto d-flex align-items-center">
 			      <a href="/">
-			      	<img src="/imgs/logo.png" height="90" alt="">
+			      	<img id="logo" src="/imgs/logo.png" height="90" alt="">
 			      </a>
 			      <div id="title-wrap">
 			      	<span id="company-name">Моё окошко</span><br>
 							<span id="what-sell">Комплектующие для окон</span>
 			      </div>
-			      <ul class="col d-flex align-items-center list-unstyled list-inline">
+			      <div class="connects-icon-wrap">
+			      	<a class="icon-link" href="#" target="_blank">
+			      		<img class="img-fluid vk-icon" src="imgs/connect/vk.svg" width="25" height="25" alt="">
+			      	</a>
+			      	<a class="icon-link" href="#" target="_blank">
+			      		<img class="img-fluid instagram-icon" src="imgs/connect/instagram.svg" width="27" height="27" alt="">
+			      	</a>
+			      	<a class="icon-link" href="#" target="_blank">
+			      		<img class="img-fluid facebook-icon" src="imgs/connect/facebook.svg" width="29" height="29" alt=""> 
+			      	</a>  
+			      	<a class="icon-link" href="#" target="_blank">
+			      		<img src="imgs/connect/odnokl.svg" width="25" height="25" alt=""> 
+			      	</a>  
+			      	<img class="whatsapp-icon" src="imgs/connect/whatsapp.svg" width="27" height="27" alt="">
+			      	<img class="viber-icon" src="imgs/connect/viber.png" width="25" height="25" alt="">
+			      	<img class="telegram-icon" src="imgs/connect/telegram.png" width="25" height="25" alt="">    
+			      </div>
+<!-- 			      <ul class="col d-flex align-items-center list-unstyled list-inline">
 			        <li class="list-inline-item"><a href="/#why-uber-section" id="why-uber-link">Главная</a></li>
 			        <li class="list-inline-item"><a href="/#req-drivers-section" id="req-drivers-link">Доставка</a></li>
 			        <li class="list-inline-item"><a href="/#req-auto-section" id="req-auto-link">Прайс-лист</a></li>
-			      </ul>
-			      <div class="col-auto d-flex align-items-center justify-content-end contacts">           
-			        <img class="phone-icon" src="imgs/phone.svg" width="17" height="17" alt="">
-			        <div class="phone-digits">+7 977 270 22 34</div>
-			        <img class="whatsapp-icon" src="imgs/whatsapp.svg" width="18" height="18" alt="">
-			        <img class="viber-icon" src="imgs/viber.png" width="16" height="18" alt="">
-			        <img src="imgs/telegram.png" width="18" height="18" alt="">           
+			      </ul> -->
+			      <a class="icon-cart" href="#" target="_blank">
+			      	<span>0</span>
+			      	<img class="img-fluid" src="imgs/connect/cart.svg" width="25" height="25" alt=""> 
+			      </a> 
+			      <div class="col-auto d-flex align-items-center justify-content-end contacts-mob-phone ml-auto">          
+			        <img class="phone-icon" src="imgs/connect/phone.svg" width="17" height="17" alt="">
+			        <div class="phone-digits">+7 495 270 22 34</div>
+        
+			      </div>
+			      <div class="col-auto d-flex align-items-center justify-content-end contacts-city-phone ml-auto">          
+			        <div class="phone-digits">+7 977 270 22 34</div>        
 			      </div>
 			    </div>
 			  </div>
@@ -56,7 +78,7 @@
 		  <a class="tile tile-lg tile-sqr tile-white ripple-effect" href="#">
 		    <span class="content-wrapper">
 		      <span class="tile-content">
-		        <span class="tile-img" style="background-image: url('imgs/tiles-squared/otkosi.jpg');"></span>
+		        <span class="tile-img" style="background-image: url('imgs/tiles/otkosi.jpg');"></span>
 		        <span class="tile-holder">
 		          <span class="title">Откосы</span>
 		        </span>
@@ -96,7 +118,7 @@
 		  <a class="tile tile-lg tile-sqr tile-white ripple-effect" href="#">
 		    <span class="content-wrapper">
 		      <span class="tile-content">
-		        <span class="tile-img" style="background-image: url(imgs/tiles-squared/uplotniteli.jpg);"></span>
+		        <span class="tile-img" style="background-image: url(imgs/tiles/uplotniteli.jpg);"></span>
 		        <span class="tile-holder">
 		          <span class="title">Уплотнители</span>
 		        </span>
@@ -136,7 +158,7 @@
 		  <a class="tile tile-lg tile-sqr tile-white ripple-effect" href="#">
 		    <span class="content-wrapper">
 		      <span class="tile-content">
-		        <span class="tile-img" style="background-image: url(imgs/tiles-squared/pilomaterialy.jpg);"></span>
+		        <span class="tile-img" style="background-image: url(imgs/tiles/pilomaterialy.jpg);"></span>
 		        <span class="tile-holder">
 		          <span class="title">Пиломатериалы</span>
 		        </span>
